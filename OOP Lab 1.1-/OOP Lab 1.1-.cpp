86b5b20558cce3442d603656b2a8135678d8c738
RomanRavlyk <Roman.Ravlyk.RI.2023@lpnu.ua>
2024-02-21 18:28:08 +0200

    ваів

--- OOP Lab 1.1-/OOP Lab 1.1-.cpp
+++ OOP Lab 1.1-/OOP Lab 1.1-.cpp
@@ -4,24 +4,33 @@ using namespace std;
 
 Fraction makeNumber(int a, int b) {
     if (b > 0) {
-        Fraction calc;
-        calc.setFirst(a);
-        calc.setSecond(b);
-        calc.Display();
-        calc.ipart();
-        return calc;
+        Fraction calculator;
+        calculator.setFirst(a);
+        calculator.setSecond(b);
+        calculator.ipart();
+        calculator.Display();
+        return calculator;
     }
     else {
         cout << "0 is uncaccpatable";
-        exit(1);
+        exit(0);
     }
 }
 
 int main() {
+    /*1*/
     Fraction calc;
-    int frst, sec;
     calc.Read();
     cout << "First num: " << calc.getFirst() << endl;
     cout << "Second num: " << calc.getSecond() << endl;
+    calc.ipart();
     calc.Display();
+    
+    /*2*/
+    Fraction calc1;
+    int first, second;
+    cout << "First = "; cin >> first;
+    cout << "Second = "; cin >> second;
+    calc1 = makeNumber(first, second);
+    return 0;
 }
